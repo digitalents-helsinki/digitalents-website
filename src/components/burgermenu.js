62b0e4a4121065e4d7979ca2f191235ab7514c3d
Vesa Merilainen <vesamatti.merilainen93@gmail.com>
2019-03-21 12:24:40 +0200

    fix burger menu

--- src/components/burgermenu.js
+++ src/components/burgermenu.js
@@ -10,28 +10,70 @@ const BurgerMenu = (props) => {
   
   const sideBarElement = visible ? 
   <SideBar>
-    <p>Etusivu</p>
-    <p>DT Media</p>
-    <p>DT ICT</p>
+    <div className="button">
+      <button onClick={handleClick}>Close</button>
+    </div>
+    <div className="links">
+      <ul>
+        <li>ETUSIVU</li>
+      </ul>
+      <ul>
+        <li>DT MEDIA</li>
+        <li>DT ICT</li>
+        <li>DT SOFTDEV</li>
+      </ul>
+      <ul>
+        <li>YHTEYSTIEDOT</li>
+        <li>TYÖPAIKAT</li>
+      </ul>
+    </div>
+    <div className="langOptions">
+      <ul>
+        <li>FI</li>
+        <li>EN</li>
+      </ul>
+    </div>
   </SideBar> : null
 
   return (
     <header>
-      <button onClick={handleClick}>Burger</button>
+      <BurgerButton onClick={handleClick}>Open</BurgerButton>
       {sideBarElement}
     </header>
   )
 }
 
+const BurgerButton = styled.button`
+  position: fixed;
+  z-index: 9;
+`
+
 const SideBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   position: fixed;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.8);
-  display: block;
   z-index: 10;
   color: white;
   opacity: 80%;
+
+  .button {
+  }
+
+  .links {
+    text-align: right;
+
+    ul {
+      padding-bottom: 1rem;
+    }
+  }
+
+  .langOptions {
+    text-align: right;
+  }
 `
 
 export default BurgerMenu
