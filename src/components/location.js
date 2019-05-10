2ea36b9009f116eae27fe83564b6ca50e8482b09
Vesa Meriläinen <vesamatti.merilainen93@gmail.com>
2019-05-10 09:51:05 +0300

    adjust location component

--- src/components/location.js
+++ src/components/location.js
@@ -13,7 +13,7 @@ const Location = props => {
     setWidth(window.innerWidth - 15)
   }
 
-  const map = `<iframe width=${width} height="350" width="100" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=24.920006990432743%2C60.16458506908792%2C24.923367798328403%2C60.16577135278478&amp;layer=mapnik&amp;marker=60.16517821628964%2C24.92168739438057"></iframe>`
+  const map = `<iframe width=${width} height="600" frameborder="0" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=24.920006990432743%2C60.16458506908792%2C24.923367798328403%2C60.16577135278478&amp;layer=mapnik&amp;marker=60.16517821628964%2C24.92168739438057"></iframe>`
   const html = { __html: map }
 
   return (
@@ -46,6 +46,7 @@ const LocationWrapper = styled.div`
   .map {
     padding-top: 25px;
     padding-bottom: 50px;
+    pointer-events: none;
   }
 `
 
