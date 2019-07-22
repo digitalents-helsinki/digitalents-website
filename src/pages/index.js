cf30fb73f2686730ad3dc7a7cf439253d3921ef6
Vesa Meriläinen <vesamatti.merilainen93@gmail.com>
2019-07-22 13:36:05 +0300

    change redirection

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 
 const IndexPage = () => {
   useEffect(() => {
-    window.location.href = 'http://digitalents.munstadi.fi/fi/'
+    window.location = 'fi/'
   })
 
   return null
